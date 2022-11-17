93d50a1d95ac441b78bffd9ec49f533ec541996e
danes44 <frumentius44@gmail.com>
2022-11-17 15:54:10 +0700

    create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// import '../App.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar flex text-white items-center p-5 sticky top-0">
+      <a className="mr-auto font-bold text-xl" href="/" target="_self" rel="noopener noreferrer">Portfolio</a>
+      <i className="bi bi-list text-3xl"></i>
+    </div>
+  );
+}
+
+export default Navbar;
